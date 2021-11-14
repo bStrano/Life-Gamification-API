9caadf40ffd4f173c8c47c135746a9b33b9dea81
Bruno Strano <bruno_strano@hotmail.com>
2021-11-14 14:58:08 -0300

    🔧 Config Validator 🔧

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { initializeSwagger } from './config/swager.config';
 import { CONFIG } from './shared/constants/CONFIG';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix(CONFIG.BASE_PATH);
   initializeSwagger(app);
   await app.listen(3000);
